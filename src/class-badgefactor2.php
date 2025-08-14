b79e7d9c2a63382c7df8ae659e9dccff5a3cedff
Miguel Legault -- ctrlweb <miguel.legault@ctrlweb.ca>
2025-08-14 13:01:05 -0400

    static to instance

--- src/class-badgefactor2.php
+++ src/class-badgefactor2.php
@@ -77,9 +77,9 @@ class BadgeFactor2 {
 
 	// Declare placeholder variables that other bf2 plugins might access. 
 	// Unless declared, From php 8.2 on, a deprecation warning for dynamic properties will ensue.
-	public static $certificates;
-	public static $courses;
-	public static $gravityforms;
+	public  $certificates;
+	public  $courses;
+	public  $gravityforms;
 
 	/**
 	 * Main Badge Factor 2 Instance.
